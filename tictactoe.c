51d1c94a51a031b004971ea647318acb7f8da7a7
Evans <69148336+evans-0@users.noreply.github.com>
2021-01-19 10:53:26 +0000

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 char l[3][3];
+
+typedef struct pos
+{
+    int x;
+    int y;
+} pos;
 
 void view(char l[3][3]);
 int isMarked(char l[3][3], int row, int column);
 void mark(char l[3][3], int row, int column, char symbol);
+void comp_mark(char l[3][3], char symbol);
 int isFinished(char l[3][3]);
 int isWin(char l[3][3]);
-int play(char name1[20], char name2[20], char sym1);
+int play1(char name1[20], char *name2, char sym1);
+int play2(char name1[20], char name2[20], char sym1);
 
 int main()
 {
-    /*
-    n1 -> player 1 name, s1 -> player 1 symbol
-    n2 -> player 2 name, s2 -> player 2 symbol
-    */
+    int ch;
+    printf("Options:\n\t1. Single Player\n\t2. Double Player");
+    printf("\nEnter choice: ");
+    scanf("%d", &ch);
+
+    if (ch==1) //Single player
+    {
+    /*n1 -> player 1 name, s1 -> player 1 symbol
+    n2 -> computer, s2 -> computer symbol*/
     char *n1 = malloc(sizeof(char));
-    char *n2 =  malloc(sizeof(char));
+    char *n2 = "Computer";
     char *s1 = malloc(sizeof(char));
     char s2;
-    printf("Enter player 1 name: ");
+    printf("Enter player name: ");
     scanf("%s", n1);
-    printf("Enter player 2 name: ");
-    scanf("%s", n2);
     printf("Enter symbol for %s(O/X): ", n1);
     scanf("%s", s1);
     
-    //n -> No. of games, w1 -> No. of games won by player 1, w2 -> No. of games won by player 2
+    //n -> No. of games, w1 -> No. of games won by player 1, w2 -> No. of games won by computer
     int n, w1 = 0, w2 = 0;
     printf("Number of games: ");
     scanf("%d", &n);
@@ -39,7 +51,7 @@ int main()
     for(int i = 1; i<=n; i++)
     {
         printf("\nMatch %d of %d\n", i, n);
-        int res = play(n1, n2, s1[0]);
+        int res = play1(n1, n2, s1[0]);
         if (res==1)
         {
             w1++;
@@ -53,9 +65,57 @@ int main()
     printf("\nScores:\n%s -> %d\n%s -> %d\n", n1, w1, n2, w2);
     printf("Thank you for playing!\n");
     free(n1);
-    free(n2);
     free(s1);
     return 0;
+    }
+    else if (ch==2) //Double player
+    {
+        char *n1 = malloc(sizeof(char));
+        char *n2 = malloc(sizeof(char));
+        char *s1 = malloc(sizeof(char));
+        char s2;
+        printf("Enter player 1 name: ");
+        scanf("%s", n1);
+        printf("Enter player 2 name: ");
+        scanf("%s", n2);
+        printf("Enter symbol for %s(O/X): ", n1);
+        scanf("%s", s1);
+        
+        //n -> No. of games, w1 -> No. of games won by player 1, w2 -> No. of games won by player 2
+        int n, w1 = 0, w2 = 0;
+        printf("Number of games: ");
+        scanf("%d", &n);
+        if (n<1)
+        {
+            printf("Please enter valid no. of games.\n");
+            return 1;
+        }
+        for(int i = 1; i<=n; i++)
+        {
+            printf("\nMatch %d of %d\n", i, n);
+            int res = play2(n1, n2, s1[0]);
+            if (res==1)
+            {
+                w1++;
+            }
+            else if (res==2)
+            {
+                w2++;
+            }
+        }
+
+        printf("\nScores:\n%s -> %d\n%s -> %d\n", n1, w1, n2, w2);
+        printf("Thank you for playing!\n");
+        free(n1);
+        free(n2);
+        free(s1);
+        return 0;
+    }
+    else
+    {
+        printf("Invalid choice!");
+        return 0;
+    }
 }
 
 //Displays the grid
@@ -88,6 +148,81 @@ int isMarked(char l[3][3], int row, int column)
 void mark(char l[3][3], int row, int column, char symbol)
 {
     l[row-1][column-1] = symbol;
+}
+
+//Computer marking
+void comp_mark(char l[3][3], char symbol)
+{
+    //Finding available positions
+    pos avail[9];
+    int c = 0;
+    for (int i = 0; i<3; i++)
+    {
+        for (int j = 0; j<3; j++)
+        {
+            if ((int) l[i][j]==45)
+            {
+                avail[c].x = i;
+                avail[c].y = j;
+                c++;
+            }
+        }
+    }
+
+    //Copying array l to array l1
+    char l1[3][3];
+    for (int i = 0; i<3; i++)
+    {
+        for (int j = 0; j<3; j++)
+        {
+            l1[i][j] = l[i][j];
+        }
+    }
+
+    char o_symbol;
+    if ((int) symbol == (int) 'X')
+    {
+        o_symbol = 'O';
+    }
+    else
+    {
+        o_symbol = 'X';
+    }
+
+     //Checking if computer won
+    for (int i = 0; i<c; i++)
+    {
+        l1[avail[i].x][avail[i].y] = symbol;
+        if (isWin(l1))
+        {
+            l[avail[i].x][avail[i].y] = symbol;
+            return;
+        }
+        else
+        {
+            l1[avail[i].x][avail[i].y] = '-';
+        }
+    }
+    //Checking if player won after player marks
+    for (int i = 0; i<c; i++)
+    {
+        l1[avail[i].x][avail[i].y] = o_symbol;
+        if (isWin(l1)) 
+        {
+            l[avail[i].x][avail[i].y] = symbol;
+            return;
+        }
+        else
+        {
+            l1[avail[i].x][avail[i].y] = '-';
+        }
+
+    }
+    srand(time(NULL));   // Initialization, should only be called once.
+    int n = rand() % c;
+    int row = avail[n].x;
+    int col = avail[n].y;
+    l[row][col] = symbol;
 }
 
 //Checks if game is finished
@@ -131,8 +266,86 @@ int isWin(char l[3][3])
     return 0;
 }
 
-//Play a game
-int play(char name1[20], char name2[20], char sym1)
+//Play a single player game
+int play1(char name1[20], char *name2, char sym1)
+{
+    char l[3][3] = {{'-','-','-'}, {'-','-','-'}, {'-','-','-'}};
+    /* name1 -> Name of player 1, sym1 -> Symbol for player 1
+       name2 -> computer, sym2 -> Symbol for computer  */
+    char sym2;
+    if ((sym1=='O') || (sym1=='o'))
+    {
+        sym1 = 'O';
+        sym2 = 'X';
+    } 
+    else
+    {
+        sym1 = 'X';
+        sym2 = 'O';
+    }
+    int i, r, c;
+    i = 0;
+    //For even numbers player 1 plays and for odd numbers player 2 plays
+    while (1)
+    {
+        if (i%2==0)
+        {
+            view(l);
+            printf("\n%s(%c) playing...\n", name1, sym1);
+            printf("Which row do you want to mark? ");
+            scanf("%d", &r);
+            printf("Which column do you want to mark? ");
+            scanf("%d", &c);
+            if (((r>3) || (c>3)) || ((r<1) || (c<1))) 
+            {
+                printf("\nEnter valid row or column.");
+            }
+            else
+            {
+                if (isMarked(l, r,c))
+                {
+                    printf("\nAlready marked\n");
+                }
+                else
+                {
+                    mark(l, r, c, sym1);
+                    i++;
+                }
+            }
+            view(l);
+            if (isWin(l))
+            {
+                printf("\n%s won!\n", name1);
+                return 1;
+            }
+        }
+        else
+        {
+            printf("\n%s(%c) playing...\n", name2, sym2);
+            comp_mark(l, sym2);
+            i++;
+            if (isWin(l))
+            {   
+                view(l);
+                printf("\n%s won!\n", name2);
+                return 2;
+            }
+        }
+        if (isFinished(l))
+        {
+            break;
+        }
+    }
+    if ((isWin(l)==0) && (isFinished(l)))
+    {
+        printf("\nGame tied!\n");
+        return 0;
+    }
+    return 0;
+}
+
+//Play a double player game
+int play2(char name1[20], char name2[20], char sym1)
 {
     char l[3][3] = {{'-','-','-'}, {'-','-','-'}, {'-','-','-'}};
     /* name1 -> Name of player 1, sym1 -> Symbol for player 1
