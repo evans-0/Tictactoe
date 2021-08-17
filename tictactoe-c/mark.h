5087bccc402ff9cd1b3db6f2b072b53c2e1145d9
Evans <9406393-evans-0@users.noreply.gitlab.com>
2021-08-17 03:57:42 +0000

    Upload New File

---
+++ tictactoe-c/mark.h
@@ -0,0 +1,13 @@
+#ifndef MARK_H
+#define MARK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "grid.h"
+#include "check.h"
+
+void mark(char *l, char symbol);
+void comp_mark(char *l, char symbol);
+
+#endif
