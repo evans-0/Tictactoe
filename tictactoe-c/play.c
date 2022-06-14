8ad177b9c2368d89751e1c9fe8f5ee7c63515eba
Evans <69148336+evans-0@users.noreply.github.com>
2022-06-14 12:45:33 +0000

    Update play.c

--- tictactoe-c/play.c
+++ tictactoe-c/play.c
@@ -22,45 +22,53 @@ int play(char *name1, char *name2, char sym1, void(*mark_func)(char*, char))
   //For even numbers player 1 plays and for odd numbers player 2 plays
   while (1)
   {
-    view(l);
-    if (i%2==0)
-    {
-      printf("\n%s(%c) playing...\n", name1, sym1);
-      mark(l, sym1);
-      i++;
-      if (isWin(l))
-      {
-		view(l);
-        printf("\n\033[38;2;0;255;0m%s won!\033[0m\n", name1);
-		system("sleep 1");
-        return 1;
-      }
-    }
-    else
-    {
-      printf("\n%s(%c) playing...\n\n", name2, sym2);
-      (*mark_func)(l, sym2);
-      i++;
-      if (isWin(l))
-      {
-        view(l);
-        printf("\n\033[38;2;0;255;0m%s won!\033[0m\n", name2);
-		system("sleep 1");
-		return 2;
-      }
-    }
-    if (isFinished(l))
-    {
       view(l);
-      break;
-    }
+      if (i%2==0){
+          nline();
+          printf("%s(%c) playing...", name1, sym1);
+          nline();
+          mark(l, sym1);
+          i++;
+          if (isWin(l)){
+              view(l);
+              nline();
+              printf("\e[38;2;0;255;0m%s won!\e[0m", name1);
+              nline();
+              system("sleep 1");
+              return 1;
+          }
+      }
+      else{
+          nline();
+          printf("%s(%c) playing...", name2, sym2);
+          nline();
+          nline();
+          (*mark_func)(l, sym2);
+          i++;
+          if (isWin(l))
+          {
+              view(l);
+              nline();
+              printf("\e[38;2;0;255;0m%s won! \e[0m", name2);
+              nline();
+              system("sleep 1");
+              return 2;
+          }
+      }
+      if (isFinished(l))
+      {
+          view(l);
+          break;
+      }
   }
   if ((isWin(l)==0) && (isFinished(l)))
   {
-    view(l);
-    printf("\n\033[38;2;100;230;200mGame tied!\033[0m\n");
-	system("sleep 1");
-    return 0;
+      view(l);
+      nline();
+      printf("\e[38;2;100;230;200mGame tied!\e[0m");
+      nline();
+      system("sleep 1");
+      return 0;
   }
   return 0;
 }
